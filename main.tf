eb050d75e6e31f0c8a9648035e3afc42a9337a52
charith.padmasiri <149340245+charith-padmasiri@users.noreply.github.com>
2025-10-17 11:06:13 +1100

    fix:test

--- main.tf
+++ main.tf
@@ -3,17 +3,15 @@ variable "AWS_SECRET_ACCESS_KEY" {
 }
 
 
+# Example 1: Public read access (existing)
+resource "aws_s3_bucket" "a_very_bad_public_s3_bucket" {
+  bucket = "my-public-bucket-3"
+  acl    = "public-read"
+}
 
 
 # Example 2: Public write access - SECURITY ISSUE
 resource "aws_s3_bucket" "dangerous_public_write_bucket" {
   bucket = "my-dangerous-write-bucket"
-  acl    = "public-read"
-}
-
-
-# Example 1: Public read access (existing)
-resource "aws_s3_bucket" "a_very_bad_public_s3_bucket" {
-  bucket = "my-public-bucket-3"
   acl    = "public-read"
 }
